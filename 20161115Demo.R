1dcb108a3d2629c7f1620ff050d35d703b1e5974
David Chiu <tr.ywchiu@gmail.com>
2016-11-15 14:42:56 +0800

    update f

--- 20161115Demo.R
+++ 20161115Demo.R
@@ -9,3 +9,11 @@ segment(code=s , jiebar=mixseg)
 ## 使用<=
 mixseg <= s
 
+## 抓出詞性
+tagseg <- worker('tag', user = '/home/trainee/user50/user.dict.utf8')
+tagseg <= s
+
+## 抓取關鍵詞 (除非有去維護IDF 字典，否則我不建議你去使用他)
+key =worker('keywords',user = '/home/trainee/user50/user.dict.utf8', topn=3)
+key <=s
+IDFPATH
